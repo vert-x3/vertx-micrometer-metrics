b44daebc7f5386ee16e4c36b2f37afe10ea60c28
Joel Takvorian <jtakvori@redhat.com>
2018-11-23 09:51:53 +0100

    PrometheusTestHelper: use logger, use vertx.setTimer, make retries configurable

--- src/test/java/io/vertx/micrometer/backend/PrometheusTestHelper.java
+++ src/test/java/io/vertx/micrometer/backend/PrometheusTestHelper.java
@@ -33,20 +33,45 @@ import java.util.Set;
  * @author Joel Takvorian
  */
 public final class PrometheusTestHelper {
+  private static final int DEFAULT_MAX_ATTEMPS = 10;
+  private static final long DEFAULT_SLEEP_BEFORE_RETRY_MS = 100;
+
   private PrometheusTestHelper() {
   }
 
   public static void tryConnect(Vertx vertx, TestContext context, int port, String host, String requestURI, Handler<Buffer> bodyReader) {
-    tryConnect(vertx, port, host, requestURI, res -> {
+    tryConnect(vertx, context, port, host, requestURI, bodyReader, DEFAULT_MAX_ATTEMPS, DEFAULT_SLEEP_BEFORE_RETRY_MS);
+  }
+
+  public static void tryConnect(Vertx vertx,
+                                TestContext context,
+                                int port,
+                                String host,
+                                String requestURI,
+                                Handler<Buffer> bodyReader,
+                                int maxAttempts,
+                                long sleepBeforeRetryMs) {
+    tryConnect(vertx, context, port, host, requestURI, res -> {
       context.assertEquals(200, res.statusCode());
       res.bodyHandler(bodyReader);
-    }, 0);
+    }, maxAttempts, sleepBeforeRetryMs, 0);
   }
 
   public static Set<String> getMetricNames(Vertx vertx, TestContext context, int port, String host, String requestURI, long timeout) {
+    return getMetricNames(vertx, context, port, host, requestURI, timeout, DEFAULT_MAX_ATTEMPS, DEFAULT_SLEEP_BEFORE_RETRY_MS);
+  }
+
+  public static Set<String> getMetricNames(Vertx vertx,
+                                           TestContext context,
+                                           int port,
+                                           String host,
+                                           String requestURI,
+                                           long timeout,
+                                           int maxAttempts,
+                                           long sleepBeforeRetryMs) {
     Async async = context.async();
     Set<String> metrics = Collections.synchronizedSet(new HashSet<>());
-    tryConnect(vertx, port, host, requestURI, resp -> {
+    tryConnect(vertx, context, port, host, requestURI, resp -> {
       context.assertEquals(200, resp.statusCode());
       RecordParser parser = RecordParser.newDelimited("\n", resp);
       parser.exceptionHandler(context::fail).endHandler(v -> {
@@ -57,32 +82,30 @@ public final class PrometheusTestHelper {
           metrics.add(line.split(" ")[2]);
         }
       });
-    }, 0);
+    }, maxAttempts, sleepBeforeRetryMs, 0);
     async.await(timeout);
     return metrics;
   }
 
   private static void tryConnect(Vertx vertx,
+                                 TestContext context,
                                  int port,
                                  String host,
                                  String requestURI,
                                  Handler<HttpClientResponse> respHandler,
+                                 int maxAttempts,
+                                 long sleepBeforeRetryMs,
                                  int attempt) {
     HttpClientRequest req = vertx.createHttpClient()
       .get(port, host, requestURI)
       .handler(respHandler)
       .exceptionHandler(e -> {
-        if (attempt < 10) {
-          System.out.println(e);
-          try {
-            Thread.sleep(500);
-          } catch (InterruptedException e1) {
-            e1.printStackTrace();
-          }
-          System.out.println("retrying...");
-          tryConnect(vertx, port, host, requestURI, respHandler, attempt + 1);
+        if (attempt < maxAttempts) {
+          vertx.setTimer(sleepBeforeRetryMs, l -> {
+            tryConnect(vertx, context, port, host, requestURI, respHandler, maxAttempts, sleepBeforeRetryMs, attempt + 1);
+          });
         } else {
-          System.out.println("aborting");
+          context.fail(e);
         }
       });
     req.end();
