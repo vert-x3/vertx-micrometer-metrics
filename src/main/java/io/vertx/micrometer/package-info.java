3105f195a8670e221f11100b588aeb9c181db7ad
Julien Viet <julien@julienviet.com>
2023-03-15 21:45:15 +0100

    Remove callback methods and use only futures

--- src/main/java/io/vertx/micrometer/package-info.java
+++ src/main/java/io/vertx/micrometer/package-info.java
@@ -14,7 +14,7 @@
  * You may elect to redistribute this code under either of these licenses.
  */
 
-@ModuleGen(name = "vertx-micrometer-metrics", groupPackage = "io.vertx", checkCallbackDeprecation = true)
+@ModuleGen(name = "vertx-micrometer-metrics", groupPackage = "io.vertx", useFutures = true)
 package io.vertx.micrometer;
 
 import io.vertx.codegen.annotations.ModuleGen;
